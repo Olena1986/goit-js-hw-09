5868148e1042eddd7a5393f95008fad94a78feae
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-04-24 16:07:56 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,26 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', startBackgroundChange);
+stopBtn.addEventListener('click', stopBackgroundChange);
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-let intervalId;
+let timeInterval = null;
 
 function startBackgroundChange() {
   startBtn.disabled = true; 
-  intervalId = setInterval(() => {
+  timeInterval = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor(); 
   }, 1000);
 }
 
 function stopBackgroundChange() {
   startBtn.disabled = false; 
-  clearInterval(intervalId); 
+  clearInterval(timeInterval); 
 }
 
 startBtn.addEventListener('click', startBackgroundChange);
